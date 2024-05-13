cca7bb264bde7a7084782f3a47ee09903a285708
Your Name <you@example.com>
2024-05-13 21:49:46 +0530

    School Crud by Shivam

---
+++ app/Models/School--all.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School--all extends Model
+{
+    use HasFactory;
+}
